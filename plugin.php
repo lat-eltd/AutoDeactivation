b626ea180140f70362155d9c512b70fb596b5879
Nicolas Schäfli <nick@fluxlabs.ch>
2022-07-26 15:22:27 +0200

    Update plugin contact infos

--- plugin.php
+++ plugin.php
@@ -6,5 +6,5 @@ $id = "autod";
 $version = "1.0.0";
 $ilias_min_version = "6.0";
 $ilias_max_version = "7.999";
-$responsible = "studer + raimann ag - Team Custom 1";
-$responsible_mail = "support@studer-raimann.ch";
+$responsible = "fluxlabs AG";
+$responsible_mail = "support@fluxlabs.ch";
