8a255efef46dac42c22bdea434dfb496eac2adda
Nicolas Schäfli <nick@fluxlabs.ch>
2022-07-26 14:56:23 +0200

    Bump up version to 1.0.0

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . "/vendor/srag/dic/src/PHPVersionChecker.php";
 
 $id = "autod";
-$version = "0.1.1";
+$version = "1.0.0";
 $ilias_min_version = "6.0";
 $ilias_max_version = "7.999";
 $responsible = "studer + raimann ag - Team Custom 1";
