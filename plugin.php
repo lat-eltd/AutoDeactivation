4665f5a10a54129f2e92be4124dddff4132310d7
Martin Studer, fluxlabs ag <martin@fluxlabs.ch>
2022-08-08 09:53:29 +0200

    1.0.1

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . "/vendor/srag/dic/src/PHPVersionChecker.php";
 
 $id = "autod";
-$version = "1.0.0";
+$version = "1.0.1";
 $ilias_min_version = "6.0";
 $ilias_max_version = "7.999";
 $responsible = "fluxlabs AG";
